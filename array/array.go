bdbd8ff5d8d0d1eb45b43f988cab72302e928962
sensir <975153482@qq.com>
2017-07-22 17:21:52 +0800

    细节调整

--- array/array.go
+++ array/array.go
@@ -3,25 +3,23 @@
  import "fmt"
  
  func main() {
-   
-   var a [5]int
-   fmt.Println("emp:", a)
-   
-   a[4] = 100
-   fmt.Println("set:", a)
-   fmt.Println("get", a[4])
-   
-   fmt.Println("len:", len(a))
-   
-   b := [5]int{1, 2, 3, 4, 5}
-   fmt.Println("dcl:", b)
-   
-   var towD [2][3]int
-   for i := 0; i < 2; i++ {
-     for j := 0; j < 3; j++ {
-       towD[i][j] = i + j
-    }
-  }
-   fmt.Println("array", towD)
-   
+   // 一维数组
+   var arr1 = [5]int{1, 2, 3, 4, 5}
+   // 多维数组 [row][col] typ
+   var arr2 = [2][3]string{{"joketorsen", "like", "fruit"},{"kitty", "like", "monkey"}}
+   fmt.Println(arr1, arr2, getAverage(arr1, 4))
 }
+
+// 向函数传递数组
+func  getAverage(arr [5]int, size int) float32 {
+   var i, sum int
+   var avg float32
+   for i = 0; i < size; i++ {
+      sum += arr[1]
+   }
+     
+  avg = float32(sum / size)
+     
+  return avg
+     
+}  
