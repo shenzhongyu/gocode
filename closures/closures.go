8a6256bfea7c64085e7de14a9e42cfd744b7d03e
sensir <975153482@qq.com>
2017-07-30 20:12:10 +0800

    Closures

---
+++ closures/closures.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func increment() func() int {
+  i := 0
+  return func() int {
+    i += 1
+    return i
+  }
+}
+
+func main(){
+  increment := increment()
+  fmt.Println(increment())
+  fmt.Println(increment())
+  fmt.Println(increment())
+  
+}
