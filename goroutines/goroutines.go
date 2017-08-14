de1a9fbf817e4a6038d1545fa1726efa427e3478
sensir <975153482@qq.com>
2017-08-14 21:40:22 +0800

    Goroutines

---
+++ goroutines/goroutines.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+  for i := 0; i < 3; i++ {
+    fmt.Println(from, ":", i)
+  }
+}
+
+func main() {
+  f("direct")
+  
+  go f("goroutine")
+  
+  go func(msg []int) {
+    fmt.Println("going:", msg)
+  }([]int{1,2,3})
+  
+  var input int
+  fmt.Println("打印变量指针:",&input)
+  fmt.Scanln(&input)
+  age := input
+  fmt.Println(age * 2)
+  fmt.Println("done")
+  
+}
+
